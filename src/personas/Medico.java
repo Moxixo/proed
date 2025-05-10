61ddfe6c652e867e1f7b9fcdb33d60b8601e16de
s3ira <s3ira@192.168.1.209>
2025-05-10 12:38:24 +0200

    Cambios clase medico

--- src/personas/Medico.java
+++ src/personas/Medico.java
@@ -1,18 +1,23 @@
 package personas;
 
-import areas.Area;
-
 /**
  *
  * @author Sheila R.
  */
 public class Medico extends Persona {
-    protected Area area;
-    protected String especialidad;
+    private String especialidad;
 
-    public Medico(String dni, String nombre, String apellido) {
+    public Medico(String dni, String nombre, String apellido, String especialidad) {
         super(dni, nombre, apellido);
+        this.especialidad = especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
     }
     
+    public String getEspecialidad() {
+        return especialidad;
+    }
     
 }
