cca98ced789f92ab6759bb6051c547c7addbe13c
Carlos David HD <Carlos David HD@DESKTOP-504BPHM>
2025-05-13 19:22:50 +0200

    AÑADIDOS Y CAMBIOS EN AFECCION Y CAMA

--- src/data/Afeccion.java
+++ src/data/Afeccion.java
@@ -8,15 +8,30 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Sheila R.
+ * @author Carlos D.HD
  */
 public class Afeccion {
+    
+    /*ATRIBUTOS*/
+    protected int gravedad; //Nivel de gravedad del 1 al 10
     protected String tipo; //Enfermedad, alergia...
     protected String nombre;
-    protected ArrayList<String> sintomas; 
-    protected int gravedad; //Nivel de gravedad del 1 al 10
-    protected ArrayList<String> tratamientos;  
+    protected ArrayList<String> sintomas;
+    protected ArrayList<String> tratamientos;
+    
+    /*CONSTRUCTORES*/
+    public Afeccion() {
+    }
 
+    public Afeccion(int gravedad, String tipo, String nombre, ArrayList<String> sintomas, ArrayList<String> tratamientos) {
+        this.gravedad = gravedad;
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.sintomas = sintomas;
+        this.tratamientos = tratamientos;
+    }
+    
+    /*GETTERS Y SETTERS*/
     public String getTipo() {
         return tipo;
     }
@@ -56,6 +71,36 @@ public class Afeccion {
     public void setTratamientos(ArrayList<String> tratamientos) {
         this.tratamientos = tratamientos;
     }
+
+    /*MÉTODOS*/
     
+    /**
+     * Éste método sirve para añadir un sintoma(String) al ArrayList de sintomas.
+     * 
+     * @param sintoma Es el parámetro de tipo String el cual tiene el valor del
+     *                              síntoma que se añadirá al ArrayList.
+     */
+    public void aniadirSintoma(String sintoma) {
+        if (!sintoma.trim().isEmpty()) {
+            sintomas.add(sintoma);
+        } else {
+            System.out.println("El síntoma no puede estar vacío.");
+        }
+    }
     
+    /**
+     * Éste método sirve para añadir un tratamiento(String) al ArrayList de tratamientos.
+     * 
+     * @param tratamiento Es el parámetro de tipo String el cual tiene el valor del
+     *                                   tratamiento que se añadirá al ArrayList.
+     */
+
+    public void aniadirTratamiento(String tratamiento) {
+        if (!tratamiento.trim().isEmpty()) {
+            sintomas.add(tratamiento);
+        } else {
+            System.out.println("El tratamiento no puede estar vacío.");
+        }
+    }
+
 }
