29c4ef03629396cf3ba25cedf6dfe2d94b4c3cf6
Moxixo <Moxixo@Moxixo-Laptop>
2025-05-13 09:06:32 +0200

    Cambios sustanciales en la estuctura de las clases: Area, Hospitalch Y Paciente \uwu/

--- src/areas/Hospital.java
+++ src/areas/Hospital.java
@@ -4,10 +4,39 @@
  */
 package areas;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author s3ira
  */
 public class Hospital {
+        
+    ArrayList <Area> areas;
+    Area urgencias = new Area(1,"Urgencias");
+    Area uci = new Area(2,"UCI");
     
+    
+    public Hospital(){
+    
+        areas = new ArrayList <Area>();
+        
+        areas.add(urgencias);
+        areas.add(uci);
+        
+    
+    }
+    
+    public void mapaHospital(){
+    
+        for (Area a : areas){
+        
+            System.out.println("=================");
+            System.out.println("=== " + a +  " ===");
+            System.out.println("=================");
+        }
+    
+    }
+     
 }
+
